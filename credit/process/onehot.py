f3ccad424b676126a66e5b204a89ddcca08a4721
xhjiang <3356569649@qq.com>
2024-07-18 18:41:11 +0800

    数据异常处理

--- credit/process/onehot.py
+++ credit/process/onehot.py
@@ -1,3 +1,6 @@
 #%%
 import pandas as pd
-pd.read_csv("../data/cs-training-woe.csv")
+df=pd.read_csv("credit/data/cs-test.csv")
+df1=pd.read_csv("credit/data/cs-training-woe.csv")
+print(df.dtypes)
+print(df1.dtypes)
